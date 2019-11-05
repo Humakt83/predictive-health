7ca65eab42aceea6501d3c7c002fb56449b2e06b
Humakt83 <urlact@yahoo.com>
2019-11-06 00:11:47 +0200

    Initial user-frontend version with questions

---
+++ frontend-client/src/components/AnswerOption.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <h2>{{answerOption.name}}</h2>
+    <div class="options-container">
+      <div class="option" :class="{'option--selected': option === selectedOption}" v-for="option in answerOption.options" v-bind:key="option" @click="selectOption(option)">
+        <span>{{ option }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'answer-option',
+  props: {
+    answerOption: {
+      type: Object,
+    }
+  },
+  data: () => {
+    return {
+      selectedOption: undefined,
+    }
+  },
+  methods: {
+    selectOption(option) {
+      this.selectedOption = option;
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+$option-width: 100px;
+$color: gray;
+$color2: aqua;
+$color3: aquamarine;
+
+
+h2 {
+  color: $color;
+  background-color: $color3;
+  padding: 0.5rem;
+  border-radius: 90px;
+  width: 300px;
+  margin: auto;
+  margin-bottom: -5px;
+}
+
+.options-container {
+  display: flex;
+  color: $color;
+  flex-direction: row;
+  justify-content: space-evenly;
+  margin-bottom: 5rem;
+  &:before {
+    content: '';
+    background-color: $color3;
+    position: absolute;
+    height: $option-width / 5;
+    width: 100%;
+    z-index: 0;
+    margin-top: $option-width / 4;
+  }
+  &:after {
+    content: '';
+    background-color: $color3;
+    position: absolute;
+    height: $option-width / 5;
+    width: 100%;
+    z-index: 0;
+    margin-top: $option-width - ($option-width / 4);
+  }
+  .option {
+    z-index: 5;
+    font-size: 1.5rem;
+    border-radius: 90px;
+    border: 5px solid $color3;
+    font-weight: bolder;
+    padding: 0 1rem;
+    background-color: $color2;
+    width: $option-width;
+    position: relative;
+    cursor: pointer;
+    &:before {
+      content: '';
+      display: block;
+      padding-top: $option-width / 2;
+    }
+    &:after {
+      content: '';
+      display: block;
+      padding-bottom: $option-width / 2;
+    }
+    &:hover {
+      background-color: $color3;
+    }
+    &.option--selected {
+      background-color: $color3;
+    }
+  }
+}
+</style>
---
+++ frontend-client/src/views/Home.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="home">
+    <h1>PREDICTIVE HEALTH</h1>
+    <answer-option v-for="answerOption in answersAndOptions" :answerOption="answerOption" :key="answerOption.name"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AnswerOption from '@/components/AnswerOption.vue'
+
+export default {
+  name: 'home',
+  components: {
+    AnswerOption
+  },
+  data: () => {
+    return {
+      answersAndOptions: [
+        {
+          name: 'Milk/Cheese',
+          options: ['Daily', 'Weekly', 'Monthly', 'Never']
+        },
+        {
+          name: 'Yoghurt/Kefir (fermented milk products)',
+          options: ['Daily', 'Weekly', 'Monthly', 'Never']
+        },
+        {
+          name: 'Red/Processed Meat',
+          options: ['Daily', 'Weekly', 'Monthly', 'Never']
+        },
+                {
+          name: 'Tea/Coffee',
+          options: ['Daily', 'Weekly', 'Monthly', 'Never']
+        },
+                {
+          name: 'Sweetened Beverages',
+          options: ['Daily', 'Weekly', 'Monthly', 'Never']
+        },
+                {
+          name: 'Fruits',
+          options: ['Daily', 'Weekly', 'Monthly', 'Never']
+        },
+                {
+          name: 'Vegetables',
+          options: ['Daily', 'Weekly', 'Monthly', 'Never']
+        },
+                {
+          name: 'Whole-grain',
+          options: ['Daily', 'Weekly', 'Monthly', 'Never']
+        },
+                {
+          name: 'Fish',
+          options: ['Daily', 'Weekly', 'Monthly', 'Never']
+        },
+                {
+          name: 'Refined Cereals',
+          options: ['Daily', 'Weekly', 'Monthly', 'Never']
+        },
+                {
+          name: 'Poultry',
+          options: ['Daily', 'Weekly', 'Monthly', 'Never']
+        },
+        {
+          name: 'Legumes',
+          options: ['Daily', 'Weekly', 'Monthly', 'Never']
+        },
+        {
+          name: 'Nuts/Seeds',
+          options: ['Daily', 'Weekly', 'Monthly', 'Never']
+        },
+        {
+          name: 'Eggs',
+          options: ['Daily', 'Weekly', 'Monthly', 'Never']
+        },
+      ],
+    }
+  }
+}
+</script>
