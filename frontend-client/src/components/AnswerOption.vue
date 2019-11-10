962a7930ba7c979ea2b1d75eacd2be6dcb92ec0c
Humakt83 <urlact@yahoo.com>
2019-11-10 23:02:31 +0200

    Some quick style changes

--- frontend-client/src/components/AnswerOption.vue
+++ frontend-client/src/components/AnswerOption.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="section-container">
     <h2>{{section}}</h2>
     <div class="options-container">
       <div class="option" :class="{'option--selected': option === selectedOption}" v-for="option in options" v-bind:key="option" @click="selectOption(option)">
@@ -34,20 +34,25 @@ export default {
 
 <style scoped lang="scss">
 
-$option-width: 100px;
-$color: gray;
-$color2: aqua;
-$color3: aquamarine;
+$option-width: 50px;
+$color: #07020d;
+$color2: #5ec2b7;
+$color3: #f1e9db;
 
+.section-container {
+  max-width: 1200px;
+  margin: auto;
+}
 
 h2 {
   color: $color;
   background-color: $color3;
   padding: 0.5rem;
   border-radius: 90px;
-  width: 300px;
+  width: 200px;
   margin: auto;
   margin-bottom: -5px;
+  font-size: 1.2rem;
 }
 
 .options-container {
@@ -55,7 +60,7 @@ h2 {
   color: $color;
   flex-direction: row;
   justify-content: space-evenly;
-  margin-bottom: 5rem;
+  margin-bottom: 1rem;
   &:before {
     content: '';
     background-color: $color3;
@@ -63,7 +68,7 @@ h2 {
     height: $option-width / 5;
     width: 100%;
     z-index: 0;
-    margin-top: $option-width / 4;
+    margin-top: $option-width / 3;
   }
   &:after {
     content: '';
@@ -72,34 +77,34 @@ h2 {
     height: $option-width / 5;
     width: 100%;
     z-index: 0;
-    margin-top: $option-width - ($option-width / 4);
+    margin-top: $option-width - ($option-width / 7);
   }
   .option {
     z-index: 5;
-    font-size: 1.5rem;
+    font-size: 1rem;
     border-radius: 90px;
-    border: 5px solid $color3;
+    border: 5px solid $color2;
     font-weight: bolder;
     padding: 0 1rem;
-    background-color: $color2;
+    background-color: $color3;
     width: $option-width;
     position: relative;
     cursor: pointer;
     &:before {
       content: '';
       display: block;
-      padding-top: $option-width / 2;
+      padding-top: $option-width / 1.75;
     }
     &:after {
       content: '';
       display: block;
-      padding-bottom: $option-width / 2;
+      padding-bottom: $option-width / 1.75;
     }
     &:hover {
       background-color: $color3;
     }
     &.option--selected {
-      background-color: $color3;
+      background-color: $color2;
     }
   }
 }
