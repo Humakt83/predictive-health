7ad4db1bbf0e202c85fd97c483595edc9dcbef3f
Risto Salama <risto.salama@gofore.com>
2019-11-13 15:56:56 +0200

    Initial version of doctor client

---
+++ frontend-doctor/src/components/PredictionResults.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="section-container" v-show="prediction">
+    <p v-for="disease in prediction" :key="disease.name">
+      {{ disease.name }} {{ disease.probability}} %
+    </p>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+export default {
+  name: 'prediction-results',
+  computed: {
+    ...mapState(['prediction'])
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+@import "../_variables.scss";
+
+</style>
