1a8928af5f842c84a1c09da60e954e22bc9e98cc
Humakt83 <urlact@yahoo.com>
2019-11-17 20:23:31 +0200

    Create background banner

---
+++ frontend-client/src/components/Banner.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="banner">
+    <div class="header">
+    PREDICTIVE HEALTH
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'banner'
+};
+</script>
+
+<style scoped lang="scss">
+
+@import "../_variables.scss";
+
+.banner {
+  background-image: url('../assets/BG.jpg');
+  background-size: 100vw 60vh;
+  height: 60vh;
+  width: 100vw;
+  position: relative;
+  
+  .header {
+    font-weight: 800;
+    font-size: 43px;
+    line-height: 77px;
+    top: 35vh;
+    left: 50vw;
+    transform: translate(-50%, 0);
+    position: absolute;
+    color: $color-background;
+  }
+}
+</style>
