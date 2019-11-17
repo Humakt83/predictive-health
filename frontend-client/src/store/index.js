c3ff74959e0c83604e314b58ffd08cf69d717528
Humakt83 <urlact@yahoo.com>
2019-11-18 00:01:34 +0200

    Convert optionValues

--- frontend-client/src/store/index.js
+++ frontend-client/src/store/index.js
@@ -15,9 +15,13 @@ export default new Vuex.Store({
   },
   actions: {
     submitAnswers: async (context, answerData) => {
+      const transformedAnswerData = answerData.map((answer) => {
+        const convertOptions = {Daily: 1, Weekly: 2, Monthly: 3, Never: 4};
+        return { name: answer.name, optionValue: convertOptions[answer.optionValue]};
+      });
       const response = await createUser();
       const userId = response.data._id;
-      await postAnswers(response.data._id, {user: userId, answers: answerData});
+      await postAnswers(response.data._id, {user: userId, answers: transformedAnswerData});
       context.commit('setAnswersToPosted');
     }
   },
