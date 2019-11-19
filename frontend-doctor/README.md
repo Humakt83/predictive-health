40192715c9edb57f0298439b48c65e6fd810038a
Humakt83 <urlact@yahoo.com>
2019-11-19 23:22:02 +0200

    Remove dead code

--- frontend-doctor/README.md
+++ frontend-doctor/README.md
@@ -1,1 +1,5 @@
 # frontend doctor
+
+Doctor client application for predictive health system.
+
+Created with Vue.js
--- frontend-doctor/src/App.vue
+++ frontend-doctor/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <home />
   </div>
 </template>
+
+<script>
+import Home from '@/views/Home';
+
+
+export default {
+  name: 'app',
+  components: {
+    Home
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -16,18 +24,5 @@
   text-align: center;
   color: #2c3e50;
   margin: auto;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
