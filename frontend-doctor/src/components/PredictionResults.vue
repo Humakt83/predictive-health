36261d8efc671962136a09e73637633f05f989fc
Humakt83 <urlact@yahoo.com>
2019-11-20 10:27:18 +0200

    Improve ui look doctor

--- frontend-doctor/src/components/PredictionResults.vue
+++ frontend-doctor/src/components/PredictionResults.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="section-container" v-show="prediction">
     <h2>PREDICTION</h2>
-    <p v-for="disease in prediction" :key="disease.name">
-      {{ disease.name }} {{ disease.probability}} %
-    </p>
+    <div class="disease" v-for="disease in prediction" :key="disease.name">
+      <div class="disease-name">
+        {{ disease.name }}
+      </div>
+      <div class="disease-value">
+        {{ disease.probability }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,6 +30,26 @@ export default {
   padding-left: 3rem;
   text-align: center;
   border-left: 3px solid $color;
+  .disease {
+    font-family: Roboto;
+    color: $color4;
+    border-radius: 15px;
+    background-color: $color;
+    padding: 8px 12px;
+    font-size: 24px;
+    margin: auto;
+    margin-bottom: 15px;
+    width: 400px;
+    .disease-name {
+      font-weight: 600;
+      display: inline;
+    }
+    .disease-value {
+      display: inline;
+      font-weight: 800;
+      margin-left: 2rem;
+    }
+  }
 }
 
 </style>
